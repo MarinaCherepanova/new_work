a04817f5df7e6f60da191060365c08ac5517e8d2
MarinaCherepanova <99033991+MarinaCherepanova@users.noreply.github.com>
2022-02-04 18:55:18 +0300

    Update дз2.1.1.py

--- дз2.1.1.py
+++ дз2.1.1.py
@@ -1,24 +1,24 @@
-# задание 1
+# Р·Р°РґР°РЅРёРµ 1
 str1 = [1,2,3,4,5,6,7,8,9,10]
 su = 0
 for i in range(len(str1)):
     su = su + str1[i]
-print ("сумма элементов списка = " + str(su))
+print ("СЃСѓРјРјР° СЌР»РµРјРµРЅС‚РѕРІ СЃРїРёСЃРєР° = " + str(su))
 
 list1 = [1,2,3,4,5,6,7,8,9,10]
 s = sum(list1)
-print("сумма элементов списка = " + str(s))
+print("СЃСѓРјРјР° СЌР»РµРјРµРЅС‚РѕРІ СЃРїРёСЃРєР° = " + str(s))
 
-# задание 2
+# Р·Р°РґР°РЅРёРµ 2
 list2 =['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n', 'z', 'z', 'z', 'a' ]
 from collections import Counter
 c = Counter(list2)
-print("дубликаты в списке:" +str([i for i, count in c.items() if count > 1]))
+print("РґСѓР±Р»РёРєР°С‚С‹ РІ СЃРїРёСЃРєРµ:" +str([i for i, count in c.items() if count > 1]))
 n = len(set(list2))
-print("количество различных элементов: "+ str(n))
-print("Топ-3: " + str(c.most_common(3)))
+print("РєРѕР»РёС‡РµСЃС‚РІРѕ СЂР°Р·Р»РёС‡РЅС‹С… СЌР»РµРјРµРЅС‚РѕРІ: "+ str(n))
+print("РўРѕРї-3: " + str(c.most_common(3)))
 
-# задание 3
+# Р·Р°РґР°РЅРёРµ 3
 picture = [
 [0,0,0,1,0,0,0],
 [0,0,1,1,1,0,0],
